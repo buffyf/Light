e60e78d025010a1b8eee052f0fa779f45345dedf
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-01-26 10:05:01 -0600

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Barbara Feinstein on 1/26/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
