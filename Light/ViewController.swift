1095717e5f46cca390ba88dcd46587fc192673f7
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-01-26 11:08:56 -0600

    change screen white to black with button

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+var lightOn = true
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        lightOn = !lightOn
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
