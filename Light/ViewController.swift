c875cbdfb2c989668b7b4353b71630b2a02ccf78
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-01-27 09:10:55 -0600

    refactor code with ternary, remove button text

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 
 class ViewController: UIViewController {
      var lightOn = true
-    @IBOutlet weak var lightButton: UIButton!
+//    @IBOutlet weak var lightButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +25,11 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
-        
+        view.backgroundColor = lightOn ? .white : .black
     }
-    
-        // Do any additional setup after loading the view, typically from a nib.
 }
+        // Do any additional setup after loading the view, typically from a nib.
+
 
 
 
